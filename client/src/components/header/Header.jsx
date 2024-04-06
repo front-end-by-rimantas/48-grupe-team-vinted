d03f756bf57f151811fc96ed52461d6617505679
AgneIlg <agneilginyte@yahoo.com>
2024-04-07 00:17:47 +0300

    header buttons

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,15 +1,36 @@
-
 import {Logo} from '../Logo/Logo';
 import { Nav } from '../nav/Nav';
 import style from './Header.module.css';
+import { VscTriangleDown } from "react-icons/vsc";
+
 
 function Header(){
     return (
         <>
         <header className={style.header}>
              <Logo />
+             
+            <form>
+                <input className={style.search} type="text" placeholder='prekės'/>
+            </form>
+            <div className={style.container}>
+            <div className={style.formRow}>
+                <button className={style.button} type="submit">Registruotis | Prisijungti</button>
+            </div>
+            <div className={style.formRow}>
+                <button className={style.button1} type="submit">Įkelti prekę</button>
+            </div>
+            </div>
+            <div className={style.formRow}>
+                <button className={style.button2} type="submit">?</button>
+            </div>
+            <div className={style.formRow}>
+                <button className={style.button3} type="submit">LT<VscTriangleDown /></button>
+            
+            </div>
+
         </header>
-         <Nav />
+        <Nav />
         </>
     );
 }
