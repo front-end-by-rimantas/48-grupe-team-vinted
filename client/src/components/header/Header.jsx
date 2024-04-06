4953245928d3defc494e137e78c1b270e379a42f
MatasMM <153711311+MatasMM@users.noreply.github.com>
2024-04-06 14:13:32 +0300

    commit

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,13 +1,12 @@
 
-import {Logo} from '../logo/Logo';
+import {Logo} from '../Logo/Logo';
 import style from './Header.module.css';
  
 function Header(){
     return (
         <>
-        <Logo />
         <header className={style.mainHeader + ' grey-bg'}>
-            <img className={style.logo} src={logo} alt="Logo" />
+             <Logo />
             <nav className={style.mainNav}>
                 <a className={style.link} href="/">Home</a>
                 <a className={style.link} href="/">About</a>
