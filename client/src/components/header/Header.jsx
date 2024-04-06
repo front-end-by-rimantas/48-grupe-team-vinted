03243dfc211cb99f6c7fa4346ec395296738cbdd
MatasMM <153711311+MatasMM@users.noreply.github.com>
2024-04-06 13:55:35 +0300

    commit

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+
+import {Logo} from '../logo/Logo';
+import style from './Header.module.css';
+ 
+function Header(){
+    return (
+        <>
+        <Logo />
+        <header className={style.mainHeader + ' grey-bg'}>
+            <img className={style.logo} src={logo} alt="Logo" />
+            <nav className={style.mainNav}>
+                <a className={style.link} href="/">Home</a>
+                <a className={style.link} href="/">About</a>
+                <a className={style.link} href="/">Contact</a>
+            </nav>
+        </header>
+        </>
+    );
+}
+ 
+export { Header };
