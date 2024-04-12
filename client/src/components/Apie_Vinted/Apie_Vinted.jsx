5a85e2045321a7beec33377bee3e065aa1094b5b
MatasMM <153711311+MatasMM@users.noreply.github.com>
2024-04-12 10:17:49 +0300

    commit

--- client/src/components/Apie_Vinted/Apie_Vinted.jsx
+++ client/src/components/Apie_Vinted/Apie_Vinted.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Apie_Vinted = () => {
   return (
     <div>
+        <React />
       <h2>About Us</h2>
       <p>Nebenešioji? Parduok!</p>
       <p>Esame pasiryžę įrodyti, kad naudotos prekės gali būti tokios pat geros kaip ir naujos. Parduok daiktus, kurių jau nebenaudoji. Įsigyk tai, ko nerasi parduotuvėse. Vinted atviras visiems, kas mano, jog kokybiški daiktai nusipelno tarnauti ilgai.</p>
